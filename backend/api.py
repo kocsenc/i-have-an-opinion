e82b30ac1069760c36c564d727b51561e091c20e
Kevin M Granger <kmg2728@rit.edu>
2015-12-07 17:23:28 -0500

    Add twitter posting

--- backend/api.py
+++ backend/api.py
@@ -1,12 +1,54 @@
 # create our little application :)
-from flask import Flask
+import configparser
+from flask import Flask, request
+import tweepy
 
 app = Flask(__name__)
 
+def are_you_the_keymaster():
+    config = configparser.ConfigParser()
+    config.read('keys.ini')
+    return config
 
-@app.route('/opinion')
+
+def twitter_handler(message):
+    twconf = are_you_the_keymaster()['twitter']
+    auth = tweepy.OAuthHandler(twconf['conkey'], twconf['consec'])
+    auth.set_access_token(twconf['atok'], twconf['atoksec'])
+    api = tweepy.API(auth)
+    return str(api.update_status(message))
+    #return str(api.verify_credentials())
+
+
+def choose_handler():
+    # TODO: add other handlers
+    def handler(message):
+        print("Sending message", str(message))
+        return twitter_handler
+    return handler
+
+
+def validate_message(message):
+    print("Testing message", message)
+    return message != ''
+
+
+def handle_message(message):
+    if not validate_message(message):
+        return 'Bad message!', 400
+
+    handler = choose_handler()
+
+    return handler(message)
+
+
+@app.route('/opinion', methods=['GET', 'POST', 'HEYLISTEN'])
 def opinionate():
-    return 'Hello World!'
+    if request.method == 'GET':
+        return 'Hey! POST me an opinion to have it put somewhere.'
+    elif request.method in ('POST', 'HEYLISTEN'):
+        message = request.data
+        return handle_message(message)
 
 
 if __name__ == '__main__':
