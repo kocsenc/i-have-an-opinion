5aecf84b3fa73b9980de78cbc6740600aa20df33
Kocsen Chung <kocsen@gmail.com>
2015-12-16 19:35:52 -0500

    Connect UI with backend

--- js/main.js
+++ js/main.js
@@ -14,7 +14,46 @@ angular
   .module('iho')
   .controller('main', MainCtrl);
 
-function MainCtrl($resource, $scope) {
+function MainCtrl($resource, $scope, $http) {
   var vm = $scope;
 
+  var couchUrl = 'http://couch.kocsen.com/opinions';
+  var Couch = $resource(couchUrl);
+
+  var url = 'http://ihoapi.kocsen.com/opinion';
+
+  vm.loading = false;
+  vm.error = false;
+  vm.userOpinion = '';
+
+  vm.shareOpinion = function () {
+    if (!vm.userOpinion) {
+      vm.error = true;
+      return
+    }
+
+    vm.loading = true;
+
+
+
+
+    couchSave(vm.userOpinion);
+  };
+
+
+  /**
+   * Save opinion to couchdb instance
+   * @param opinion
+   */
+  function couchSave(opinion) {
+    var couch = new Couch({
+      opinion: opinion,
+      created_at: new Date()
+    });
+
+    couch.$save();
+  }
+
+  $('.ui.accordion').accordion();
+  $('#help').popup();
 }
